1232badea0e036339ce414a399e360402afb53aa
Michael Weiss <michael.weiss@bayer.com>
2022-01-19 16:27:28 +0100

    Some code clean up and documentation done

--- src/main/java/de/code2be/pdfsplit/ui/swing/actions/OpenFileAction.java
+++ src/main/java/de/code2be/pdfsplit/ui/swing/actions/OpenFileAction.java
@@ -8,14 +8,18 @@ import javax.swing.JFileChooser;
 import de.code2be.pdfsplit.ui.swing.FileExtensionFilter;
 import de.code2be.pdfsplit.ui.swing.PDFSplitFrame;
 
+/**
+ * The UI action that is responsible for opening a PDF file for splitting.
+ * 
+ * @author Michael Weiss
+ *
+ */
 public class OpenFileAction extends BasicAction
 {
 
     private static final long serialVersionUID = 8386886689858092404L;
 
     private final PDFSplitFrame mFrame;
-
-    private File mDirectory;
 
     public OpenFileAction(PDFSplitFrame aFrame)
     {
@@ -24,18 +28,54 @@ public class OpenFileAction extends BasicAction
     }
 
 
+    /**
+     * 
+     * @return the directory that should be initially selected on open.
+     */
     protected File getCurrentDirectory()
     {
-        if (mDirectory == null)
+        String dir = mFrame.getConfig()
+                .getProperty(PDFSplitFrame.PROP_DIRECTORY_OPEN);
+        if (dir != null)
         {
-            String dir = mFrame.getConfig()
-                    .getProperty(PDFSplitFrame.PROP_DIRECTORY_OPEN);
-            if (dir != null)
-            {
-                mDirectory = new File(dir);
-            }
+            return new File(dir);
         }
-        return mDirectory;
+        return null;
+    }
+
+
+    /**
+     * Ask the user which file to be opened.
+     * 
+     * @return the file that is to be opened.
+     */
+    protected File getFile2Open()
+    {
+        JFileChooser chooser = new JFileChooser();
+        chooser.addChoosableFileFilter(new FileExtensionFilter(".pdf",
+                getMessageForSubKey("pdfFilter")));
+        chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        chooser.setAcceptAllFileFilterUsed(false);
+        File curDir = getCurrentDirectory();
+        if (curDir != null)
+        {
+            chooser.setCurrentDirectory(curDir);
+        }
+
+        if (chooser.showOpenDialog(mFrame) != JFileChooser.APPROVE_OPTION)
+        {
+            return null;
+        }
+
+        File f = chooser.getSelectedFile();
+        if (f == null)
+        {
+            return null;
+        }
+        File dir = f.getParentFile();
+        mFrame.getConfig().put(PDFSplitFrame.PROP_DIRECTORY_OPEN,
+                dir.getAbsolutePath());
+        return f;
     }
 
 
@@ -48,30 +88,19 @@ public class OpenFileAction extends BasicAction
             return;
         }
 
-        JFileChooser chooser = new JFileChooser();
-        chooser.addChoosableFileFilter(new FileExtensionFilter(".pdf",
-                getMessageForSubKey("pdfFilter")));
-        chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-        chooser.setAcceptAllFileFilterUsed(false);
-        File curDir = getCurrentDirectory();
-        if (curDir != null)
-        {
-            chooser.setCurrentDirectory(curDir);
-        }
-        if (chooser.showOpenDialog(mFrame) != JFileChooser.APPROVE_OPTION)
-        {
-            return;
-        }
-
-        File f = chooser.getSelectedFile();
+        File f = getFile2Open();
         if (f == null)
         {
             return;
         }
-        File dir = f.getParentFile();
-        mDirectory = dir;
+        // we call open in a separate daemon thread. This ensures non blocking
+        // UI for big documents.
+        Thread t = new Thread(() -> {
+            mFrame.openPDFFile(f);
+        });
+        t.setDaemon(true);
+        t.start();
 
-        mFrame.openPDFFile(f);
     }
 
 }
